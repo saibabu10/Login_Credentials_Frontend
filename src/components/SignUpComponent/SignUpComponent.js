a832ddcb739bf5df619a559f7f8270b85575b1cf
sai babu <saibabu@sais-Air-2.domain.name>
2023-06-11 00:27:17 +0530

    ADD.Forms and State and console the data

--- src/components/SignUpComponent/SignUpComponent.js
+++ src/components/SignUpComponent/SignUpComponent.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
 
  class SignUpComponent extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            firstname:"",
+            lastname:"",
+            email:"",
+            password:""
+        }
+    }
+    firstnameHandler = event =>{
+        this.setState({
+            firstname:event.target.value
+        })
+    }
+    lasttnameHandler = event =>{
+        this.setState({
+            lasttname:event.target.value
+        })
+    }
+    emailHandler = event =>{
+        this.setState({
+            email:event.target.value
+        })
+    }
+    passwordHandler = event =>{
+        this.setState({
+            password :event.target.value
+        })
+    }
+
+    formSubmitHandler = event =>{
+    event.preventDefault()
+    console.log(this.state.firstname,
+        this.state.lastname,
+        this.state.email,
+        this.state.password);
+}
   render() {
+    const {firstname,lastname,email,password} = this.state
     return (
-     <form>
+     <form onSubmit={this.formSubmitHandler}>
         <h3>Sign Up</h3>
         <div className='mb-3'>
             <label>First Name</label>
@@ -11,6 +49,8 @@ import React, { Component } from 'react'
             type='text'
             className='form-control'
             placeholder='Enter Your First Name'
+            value={firstname}
+            onChange={this.firstnameHandler}
             required/> 
         </div>
         <div className='mb-3'>
@@ -18,7 +58,9 @@ import React, { Component } from 'react'
             <input
             type='text'
             className='form-control'
-            placeholder='Enter Your First Name'
+            placeholder='Enter Your Last Name'
+            value={lastname}
+            onChange={this.lastnameHandler}
             required/> 
         </div>
         <div className='mb-3'>
@@ -26,7 +68,9 @@ import React, { Component } from 'react'
             <input
             type='email'
             className='form-control'
-            placeholder='Enter Your First Name'
+            placeholder='Enter Your Email'
+            value={email}
+            onChange={this.emailHandler}
             required/> 
         </div>
         <div className='mb-3'>
@@ -34,7 +78,9 @@ import React, { Component } from 'react'
             <input
             type='password'
             className='form-control'
-            placeholder='Enter Your First Name'
+            placeholder='Enter The Password'
+            value={password}
+            onChange={this.passwordHandler}
             required/> 
         </div>
         <div className='d-grid'>
